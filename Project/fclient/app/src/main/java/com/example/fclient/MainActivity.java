f886f648d5b8a44cbe67a9af76b6bfeee0643996
Kate <khokhlacheva02@bk.ru>
2023-03-19 03:20:12 +0300

    2nd Lab Complete

--- Project/fclient/app/src/main/java/com/example/fclient/MainActivity.java
+++ Project/fclient/app/src/main/java/com/example/fclient/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fclient.databinding.ActivityMainBinding;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityResultLauncher activityResultLauncher;
+
+
 
     // Used to load the 'fclient' library on application startup.
     static {
@@ -27,10 +43,63 @@ public class MainActivity extends AppCompatActivity {
         int res = initRng();
         byte[] v = randomBytes(10);
 
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        /*TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());*/
     }
+
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+
+    public void onButtonClick(View v)
+    {
+        /*byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+        byte[] dec = decrypt(key, enc);
+        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+
+
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();*/
+
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+    }
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
+
+
+
+
+
 
     /**
      * A native method that is implemented by the 'fclient' native library,

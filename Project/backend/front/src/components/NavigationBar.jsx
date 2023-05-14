a2eb6c6b344646b81d017108716c309e649dffb7
Kate <khokhlacheva02@bk.ru>
2023-05-14 19:13:48 +0300

    Lab10

--- Project/backend/front/src/components/NavigationBar.jsx
+++ Project/backend/front/src/components/NavigationBar.jsx
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import Utils from "../utils/Utils";
 import BackendService from "../services/BackendService";
+import {connect} from "react-redux";
+import {userActions} from "../utils/Rdx";
 
 class NavigationBarClass extends React.Component {
 
@@ -16,12 +18,16 @@ class NavigationBarClass extends React.Component {
         this.logout = this.logout.bind(this)
     }
 
+
     logout() {
-        BackendService.logout().then(() => {
-            Utils.removeUser();
-            this.goHome()
-        });
+        BackendService.logout()
+            .then(() => {
+                Utils.removeUser();
+                this.props.dispatch(userActions.logout())
+                this.props.navigate('Login');
+            })
     }
+
 
 
     goHome() {
@@ -32,29 +38,33 @@ class NavigationBarClass extends React.Component {
 
 
 
+
     render() {
-        let uname = Utils.getUserName();
         return (
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
+                <Navbar.Brand>myRPO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto">
+                        {/*<Nav.Link href="/home">Home</Nav.Link>*/}
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link onClick={this.goHome}>Another Home</Nav.Link>
-                        <Nav.Link onClick={() =>{ this.props.navigate("\home")}}>Yet Another Home</Nav.Link>
+                        <Nav.Link onClick={this.goHome}>Another home</Nav.Link>
+                        <Nav.Link onClick={() => { this.props.navigate("home")}} >Yet another home</Nav.Link>
                     </Nav>
-                    <Navbar.Text>{uname}</Navbar.Text>
-                    { uname &&
-                        <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
-                    }
-                    { !uname &&
-                        <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
-                    }
                 </Navbar.Collapse>
+                <Navbar.Text>{this.props.user && this.props.user.login}</Navbar.Text>
+                { this.props.user &&
+                    <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
+                }
+                { !this.props.user &&
+                    <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
+                }
             </Navbar>
         );
     }
+
+
 
 }
 
@@ -64,4 +74,15 @@ const NavigationBar = props => {
     return <NavigationBarClass navigate={navigate} {...props} />
 }
 
-export default  NavigationBar;
+
+function mapStateToProps(state) {
+    const { user } = state.authentication;
+    return { user };
+}
+
+export default  connect(mapStateToProps)(NavigationBar);
+
+
+
+
+//export default  NavigationBar;
